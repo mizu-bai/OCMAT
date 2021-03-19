e92d262a534d9d2d231bdfee95120e72b29259a1
mizu-bai <shiragwa4519@outlook.com>
2021-03-19 12:44:21 +0800

    Update

---
+++ OCMAT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCMAT
+//
+//  Created by mizu-bai on 2021/3/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
